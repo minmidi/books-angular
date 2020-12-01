4a70dbe0c77e12ab4c140e4784278e917dfaa903
minmidi <letruonggiang0807@gmail.com>
2020-12-02 04:29:10 +0700

    update

--- src/app/pages/modules/categories/categories.component.ts
+++ src/app/pages/modules/categories/categories.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {CdkTextareaAutosize} from '@angular/cdk/text-field';
+import { Component, NgZone, OnInit, ViewChild} from '@angular/core';
+import {take} from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-categories',
@@ -6,8 +9,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./categories.component.css']
 })
 export class CategoriesComponent implements OnInit {
+  heros = [
+    {
+      id: 1,
+      first: 'Lê',
+      last: 'Trường',
+      handle: 'Giang',
+    },
+    {
+      id: 1,
+      first: 'Lê',
+      last: 'Thị',
+      handle: 'Hiền',
+    },
+    {
+      id: 1,
+      first: 'Lê',
+      last: 'Quỳnh',
+      handle: 'Trang',
+    }
+  ]
 
-  constructor() { }
+  // tslint:disable-next-line: variable-name
+  constructor(private _ngZone: NgZone) { }
+  @ViewChild('autosize')
+  autosize!: CdkTextareaAutosize;
+
+  // tslint:disable-next-line: typedef
+  triggerResize() {
+    // Wait for changes to be applied, then trigger textarea resize.
+    this._ngZone.onStable.pipe(take(1))
+        .subscribe(() => this.autosize.resizeToFitContent(true));
+  }
 
   ngOnInit(): void {
   }
